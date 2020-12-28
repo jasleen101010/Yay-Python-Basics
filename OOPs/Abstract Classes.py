f6fc9cc5254cfb98676ad5041a06c8252abc6f31
Jasleen Sondhi <jasleensondhi@gmail.com>
2020-12-28 21:22:33 +0530

    Encapsulation and Abstraction Programs

--- OOPs/Abstract Classes.py
+++ OOPs/Abstract Classes.py
@@ -1,54 +1,49 @@
 # Python program showing
 # abstract base class work
-
 from abc import ABC, abstractmethod
-
-
 class Polygon(ABC):
 
     # abstract method
-    def noofsides(self):
+    def sides(self):
         pass
-
 
 class Triangle(Polygon):
 
     # overriding abstract method
-    def noofsides(self):
+    def sides(self):
         print("I have 3 sides")
 
 
 class Pentagon(Polygon):
 
     # overriding abstract method
-    def noofsides(self):
+    def sides(self):
         print("I have 5 sides")
 
 
 class Hexagon(Polygon):
 
     # overriding abstract method
-    def noofsides(self):
+    def sides(self):
         print("I have 6 sides")
 
 
 class Quadrilateral(Polygon):
 
     # overriding abstract method
-    def noofsides(self):
+    def sides(self):
         print("I have 4 sides")
 
     # Driver code
 
-
 R = Triangle()
-R.noofsides()
+R.sides()
 
 K = Quadrilateral()
-K.noofsides()
+K.sides()
 
 R = Pentagon()
-R.noofsides()
+R.sides()
 
 K = Hexagon()
-K.noofsides()
+K.sides()
